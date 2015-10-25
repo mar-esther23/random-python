aaf05ae1464ce5a88d38a3b8d9aafe70c4a128cc
mar-esther23 <mar.esther23@gmail.com>
2015-10-25 11:38:04 -0600

    Python script to run an Rscript and parse the resulting file

--- runRscript.py
+++ runRscript.py
@@ -1,2 +1,32 @@
 
-import sys
+import os.path
+import subprocess
+
+rscript = "GRNattr.R"
+input_file = "grn.dat"
+temp_file = "grn_attr.dat"
+
+# run command:
+#   Rscript GRNattr.R grn.dat grn_attr.dat
+# create file grn_attr.dat with results
+subprocess.check_call(["Rscript", rscript, input_file, temp_file])
+
+# check if output file exists
+if not os.path.isfile("grn_attr.dat"):
+    raise IOError("Error with " +rscript +" output file "+temp_file)
+
+# parse output of rscipt
+try: 
+    with open(temp_file, 'r') as f:
+        data = f.read()
+    data = data.split('\n')
+    data = [d.strip().split('\t') for d in data]
+except:
+    raise IOError("Error with " +rscript +" output file "+temp_file)
+# remove temp file
+finally:  os.remove(temp_file)
+
+# do something with the results
+for d in data[0:-1]: print d
+
+
